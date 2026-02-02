bf74f2128d684119ffc7efa9123436415f1c66fa
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2026-02-02 18:39:31 -0300

    fix
    Separação de repositorios. esse é um app q uso um pacote  tb feito por mim do meu git, e o outro e somente um pacote, oq fiz foi excluir a pasta do pacote de dentro desse repo e usar ele como dependencia

--- camera_overlay/lib/camera_overlay.dart
+++
@@ -1,5 +0,0 @@
-library camera_overlay;
-
-export 'src/camera/page.dart';
-export 'src/camera/model/localizacao.dart';
-export 'src/camera/enum.dart';
--- camera_overlay/lib/src/camera/enum.dart
+++
@@ -1,8 +0,0 @@
-enum CameraStatus {
-  loading,
-  // semMissao,
-  permissaoNegada,
-  inicializandoCamera,
-  pronta,
-  erro,
-}
